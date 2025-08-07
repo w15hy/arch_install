1e42b30752be601f88cfdaa140c6e1ffb7a856bb
w15hy <164117598+w15hy@users.noreply.github.com>
2025-08-06 22:08:30 -0500

    Update arch_install3.sh

--- arch_install3.sh
+++ arch_install3.sh
@@ -1,10 +1,15 @@
-set -e 
+set -e
 
 yay -S proton-pass-bin --noconfirm
 pacman -S pavucontrol locate pipewire pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
 pacman -S flameshot rofi --noconfirm
 pacman -S pavucontrol pipewire-audio pipewire-alsa pipewire-pulse --noconfirm
-pacman -S xf86-video-amdgpu
-pacman -S mesa lib32-mesa
+pacman -S xf86-video-amdgpu --noconfirm
+pacman -S 7zip acpi amd-ucode baobab feh flameshot gnome-themes-extra gvfs gvfs-mtp gvfs-nfs koodo-reader-bin lazygit mpd ncmpcpp npm obsidian packettracer pcmanfm picom polybar ttf-jetbrains-mono-nerd unzip xclip zip --noconfirm
+pacman -S mesa lib32-mesa --noconfirm
+pacman -S qemu-full qemu-img libvirt virt-install virt-manager virt-viewer edk2-ovmf dnsmasq swtpm guestfs-tools libosinfo tuned --noconfirm
 
-mv ./resources/screen.sh ~/.config/ # a;adir esto al final del /etc/ly/setup.sh
+rm -r /etc/ly/setup.sh
+rm -r /etc/ly/config.ini
+
+mv ./resources/config.ini ./resources/screen.sh ./resources/setup.sh /etc/ly/
